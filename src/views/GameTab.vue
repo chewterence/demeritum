1de2c17d1435309e9e91b66c76f12ef2a24b5ee5
Terence Chew <chewterence@hotmail.com>
2020-10-22 13:51:12 +0800

    updated the game

--- src/views/GameTab.vue
+++ src/views/GameTab.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
   <div>
-<iframe frameborder="0" src="https://itch.io/embed-upload/2829934?color=333333" allowfullscreen="" width=100% height="1600"><a href="https://chewterence.itch.io/test">Play demeritumtest on itch.io</a></iframe>
+<iframe frameborder="0" src="https://itch.io/embed-upload/2868847?color=333333" allowfullscreen="" width="100%" height="1600"><a href="https://chewterence.itch.io/test">Play demeritumtest on itch.io</a></iframe>
 </div>
   </div>
 </template>
